389de6c6ee7d97ce3b8eba848008e5a5af472a61
apple <a.oktisyuk@gmail.com>
2021-04-07 22:18:49 +0300

    start to added video

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,24 @@
 
+
+function toggleClass() {
+	const elements = document.querySelectorAll('[data-toggle-class]');
+	if (!elements.length) {
+		return null;
+	}
+	
+	for (let el of elements) {
+		el.addEventListener('click', function (e) {
+			e.preventDefault();
+			const wrapId = this.dataset.toggleClass;
+			const wrap = document.querySelector(wrapId);
+			if (!wrap) return;
+			
+			wrap.classList.toggle('active');
+		})
+	}
+}
+
 (function () {
 	console.log("START");
+	toggleClass();
 })();
